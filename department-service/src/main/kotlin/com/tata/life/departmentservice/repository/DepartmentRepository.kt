74a1e32270697d92d9e5788a009b2ccae12c08fa
Matsuki Jinen <matsukijinen@matsugishizens-MacBook-Air.local>
2022-11-29 15:23:06 +0900

    add Repository annotation

--- department-service/src/main/kotlin/com/tata/life/departmentservice/repository/DepartmentRepository.kt
+++ department-service/src/main/kotlin/com/tata/life/departmentservice/repository/DepartmentRepository.kt
@@ -2,7 +2,9 @@ package com.tata.life.departmentservice.repository
 
 import com.tata.life.departmentservice.entity.Department
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.stereotype.Repository
 
+@Repository
 interface DepartmentRepository: JpaRepository<Department, Long> {
     fun findByDepartmentId(id: Long): Department
 }
