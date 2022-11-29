f7fd4a85c349edb9c649ebbfa6bbe7e1f79fe0e8
Matsuki Jinen <matsukijinen@matsugishizens-MacBook-Air.local>
2022-11-29 13:39:12 +0900

    create repository

---
+++ department-service/src/main/kotlin/com/tata/life/departmentservice/repository/DepartmentRepository.kt
@@ -0,0 +1,7 @@
+package com.tata.life.departmentservice.repository
+
+import com.tata.life.departmentservice.entity.Department
+import org.springframework.data.jpa.repository.JpaRepository
+
+interface DepartmentRepository: JpaRepository<Department, Long> {
+}
